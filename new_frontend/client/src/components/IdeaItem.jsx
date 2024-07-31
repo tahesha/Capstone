9810f1608068fbd57ea72ad82c4a5af76cf5debe
Tahesha <taheshao@gmail.com>
2024-07-30 21:35:29 -0400

    Added Authors.

--- new_frontend/client/src/components/IdeaItem.jsx
+++ new_frontend/client/src/components/IdeaItem.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import IdeaAuthor from './IdeaAuthor'
 
-const IdeaItem = ({postID, category, title, description, authorID, thumbnail}) => {
+const IdeaItem = ({ideaID, category, title, description, authorID, thumbnail}) => {
     const shortDescription = description.length > 145 ? description.substr(0, 145) + '...' : description;
-    const postTitle = title.length > 30 ? title.substr(0, 30) + '...' : title;
+    const ideaTitle = title.length > 30 ? title.substr(0, 30) + '...' : title;
 
     return (
-    <article className="post">
-        <div className="post_thumbnail">
+    <article className="idea">
+        <div className="idea_thumbnail">
             <img src={thumbnail} alt={title} />
         </div>
-        <div className="post_content">
-            <Link to={`/Ideas/${postID}`}>
-            <h3>{postTitle}</h3>
+        <div className="idea_content">
+            <Link to={`/Ideas/${ideaID}`}>
+            <h3>{ideaTitle}</h3>
             </Link>
             <p>{shortDescription}</p>
             <div className="Ideas_footer">
--- new_frontend/client/src/pages/Authors.jsx
+++ new_frontend/client/src/pages/Authors.jsx
@@ -8,10 +8,10 @@ import { Link } from 'react-router-dom'
 
 const authorsData = [
 {id: 1, avatar: Avatar1, name: 'Kenya Laren', Ideas: 3},
-{id: 2, avatar: Avatar2, name: 'Jane Doe', Ideas: 5},
-{id: 3, avatar: Avatar3, name: 'Dramani Mahama', Ideas: 0},
-{id: 4, avatar: Avatar4, name: 'Nana Addo', Ideas: 2},
-{id: 5, avatar: Avatar5, name: 'Hajia Bintu', Ideas: 1},
+{id: 2, avatar: Avatar2, name: 'Nyrobi Dunee', Ideas: 5},
+{id: 3, avatar: Avatar3, name: 'Zyaire Umbe', Ideas: 0},
+{id: 4, avatar: Avatar4, name: 'Aiyana Mbori', Ideas: 2},
+{id: 5, avatar: Avatar5, name: 'Chidera Foluke', Ideas: 1},
 ]
 
 
