1f4869fbee01f35f834ebcaa35aeebfd7e0c09ff
Tahesha <taheshao@gmail.com>
2024-07-30 21:37:27 -0400

    Added CatogoryIdeas.

--- new_frontend/client/src/pages/CategoryIdeas.jsx
+++ new_frontend/client/src/pages/CategoryIdeas.jsx
@@ -9,7 +9,7 @@ const CategoryIdeas = () => {
     {Ideas.length > 0 ? <div className="container post_container">
               {
                   Ideas.map(({ id, thumbnail, category, title, desc, authorID }) =>
-                      <IdeaItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} description={desc} authorID={authorID} />)
+                      <IdeaItem key={id} ideaID={id} thumbnail={thumbnail} category={category} title={title} description={desc} authorID={authorID} />)
               }
     </div> : <h2 className='center'>No Ideas found</h2>}
 </section> 
